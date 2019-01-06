551d7bdf4186f30ca7c38396ae2202683ca5617c
Dennis Tjahyadi <dennis.tgt@gmail.com>
2019-01-06 16:36:56 +0700

    changed

--- app/src/main/java/com/dovoo/travelbuddy/RegisterActivity.java
+++ app/src/main/java/com/dovoo/travelbuddy/RegisterActivity.java
@@ -5,9 +5,21 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.tsongkha.spinnerdatepicker.DatePicker;
+import com.tsongkha.spinnerdatepicker.DatePickerDialog;
+import com.tsongkha.spinnerdatepicker.SpinnerDatePickerDialogBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RegisterActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private EditText etName, etEmail, etBirthday, etGender;
+    private Calendar calendarBirthdate = Calendar.getInstance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         init();
     }
+
     private void init() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,7 +35,37 @@ public class RegisterActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("Sign up");
         }
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etBirthday = findViewById(R.id.etBirthday);
+        etGender = findViewById(R.id.etGender);
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendarBirthdate.set(year,monthOfYear,dayOfMonth);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy");
+                etBirthday.setText(sdf.format(calendarBirthdate.getTime()));
+            }
+        };
+
+        final DatePickerDialog birthdateDialog = new SpinnerDatePickerDialogBuilder()
+                .context(RegisterActivity.this)
+                .callback(dateSetListener)
+                .spinnerTheme(R.style.NumberPickerStyle)
+                .showTitle(true)
+                .showDaySpinner(true)
+                .defaultDate(Calendar.getInstance().get(Calendar.YEAR)-18, 0, 1)
+                .maxDate(Calendar.getInstance().get(Calendar.YEAR)-18, 0, 1)
+                .build();
+
+        etBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                birthdateDialog.show();
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/com/dovoo/travelbuddy/VerificationEmailActivity.java
@@ -0,0 +1,14 @@
+package com.dovoo.travelbuddy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class VerificationEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_email);
+    }
+}
